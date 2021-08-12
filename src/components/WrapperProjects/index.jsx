2c853497e7f06de9ee33a364dc3aa76e9a770cfe
Pedro Candido <pedrocandido@MacBook-Pro-de-Pedro.local>
2021-08-11 21:51:16 -0300

    feat: projects wrapper

--- src/components/WrapperProjects/index.jsx
+++ src/components/WrapperProjects/index.jsx
@@ -1,6 +1,6 @@
-import { ProjectsContainer } from "./style"
+import { ProjectsContainer, ProjectsList, BestProject } from "./style"
 
-const SectionTitle = ({children}) => {
+const SectionTitle = ({ children }) => {
     return (
         <>
             <h1>
@@ -12,33 +12,55 @@ const SectionTitle = ({children}) => {
 
 const projects = [
     {
-        image: '',
-        link: '',
+        image: 'https://place-hold.it/200x200',
+        link: 'https://github.com/pedro-candido',
         name: 'Projeto 1',
     },
     {
-        image: '',
-        link: '',
+        image: 'https://place-hold.it/200x200',
+        link: 'https://github.com/pedro-candido',
         name: 'Projeto 2'
     },
     {
-        image: '',
-        link: '',
+        image: 'https://place-hold.it/200x200',
+        link: 'https://github.com/pedro-candido',
         name: 'Projeto 3'
     },
     {
-        image: '',
-        link: '',
-        name: 'Projeto 4'
+        image: 'https://place-hold.it/800x200',
+        link: 'https://github.com/pedro-candido',
+        name: 'Projeto 4',
+        isBest: true
     }
 ]
+
+const baseProjects = projects.filter((item) => !item.isBest)
+const bestProject = projects.filter((item) => item.isBest)[0]
 
 export const WrapperProjects = () => {
     return (
         <ProjectsContainer>
             <SectionTitle>
-                Meus projetos
+                Meus Projetos
             </SectionTitle>
+            <ProjectsList>
+                {baseProjects.map((item) => {
+                    if (!item.isBest)
+                        return (
+                            <li key={item.name} >
+                                <a href={item.link}>
+                                    <img src={item.image} alt={item.name} />
+                                </a>
+                                <h2>{item.name}</h2>
+                            </li>
+                        )
+                })}
+            </ProjectsList>
+
+            <BestProject>
+                <img src={bestProject.image} alt={bestProject.name} />
+                <h2>{bestProject.name}</h2>
+            </BestProject>
         </ProjectsContainer>
     )
 }
--- src/components/WrapperProjects/style.js
+++ src/components/WrapperProjects/style.js
@@ -6,4 +6,29 @@ export const ProjectsContainer = styled.div`
     margin: auto;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+`
+
+export const ProjectsList = styled.ul`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
+    list-style-type: none;
+
+    li{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+export const BestProject = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
 `
